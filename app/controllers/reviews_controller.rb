23293b82a0e4206deea04175b6d9e411bb7916ed
Corentin Dubois <corentindubois22@gmail.com>
2024-08-28 14:01:03 -0600

    done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,13 @@
 class ReviewsController < ApplicationController
   before_action :set_restaurant, only: %i[new create]
 
-  def new
-    @review = Review.new
-  end
-
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new, status: :unprocessable_entity
+      render "restaurants/show", restaurant: @restaurant, status: :unprocessable_entity
     end
   end
 
